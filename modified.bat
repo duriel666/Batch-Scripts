5db6a9c1128bdf13688ddfa271d03e3c2c45c4de
duriel666 <janne.malaska@outlook.com>
2024-08-19 08:16:22 +0300

    added option for time as comments

--- modified.bat
+++ modified.bat
@@ -8,12 +8,18 @@ REM Loop through all files in current folder with the specified extension
 for %%F in ("*.!ext!") do (
     REM Extract year, month and day from filename formatted as "(2024-08-19) something.mp4"
     set "filename=%%~nF"
+
     set "year=!filename:~1,4!"
     set "month=!filename:~6,2!"
     set "day=!filename:~9,2!"
 
+    REM set "hour=!filename:~12,2!"
+    REM set "minute=!filename:~15,2!"
+    REM set "second=!filename:~18,2!"
+
     echo modifying file !filename!.!ext!
-    powershell -command "(Get-Item '!filename!.!ext!').LastWriteTime = '!year!-!month!-!day!'"
+    powershell -command "(Get-Item '!filename!.!ext!').LastWriteTime = '!year!-!month!-!day! 12:00:00'"
+    REM powershell -command "(Get-Item '!filename!.!ext!').LastWriteTime = '!year!-!month!-!day! !hour!:!minute!:!second!'"
 )
 
 echo Done
