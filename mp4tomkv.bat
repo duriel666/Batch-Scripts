4798ccd4e4aeea5653895c26c6ec0027056d4c03
duriel666 <janne.malaska@outlook.com>
2025-08-28 03:13:38 +0300

    Convert mp4 to mkv with ffmpeg

---
+++ mp4tomkv.bat
@@ -0,0 +1,20 @@
+@echo off
+setlocal enabledelayedexpansion
+
+REM Usage: just run in folder
+
+pushd %~dp0 || (
+    echo Error: Could not map the UNC path
+    pause
+    exit /b 1
+)
+
+echo Directory: %cd%
+
+for %%F in (*.mp4) do (
+    echo Converting file %%F ...
+    ffmpeg -i "%%F" -c copy "%%~nF.mkv"
+)
+
+echo Done
+endlocal
